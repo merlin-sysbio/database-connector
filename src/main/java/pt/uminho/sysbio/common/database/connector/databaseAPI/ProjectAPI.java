157c8037c87e991b8e42f38f325b2dee1d42a651
Davide Lagoa <davidelagoa@hotmail.com>
2018-02-10 20:58:40 +0000

    corrections

--- src/main/java/pt/uminho/sysbio/common/database/connector/databaseAPI/ProjectAPI.java
+++ src/main/java/pt/uminho/sysbio/common/database/connector/databaseAPI/ProjectAPI.java
@@ -835,12 +835,7 @@ public class ProjectAPI {
 				"RIGHT JOIN pathway_has_reaction ON pathway_idpathway=pathway.idpathway " +
 				"GROUP BY pathway_idpathway ORDER BY name;");
 
-		System.out.println(qls.keySet());
-
 		while(rs.next()) {
-
-			System.out.println("pathID ----->"+ rs.getString(1));
-			System.out.println("count ----->"+ rs.getString(2));
 
 			qls.get(rs.getString(1))[2] = rs.getString(2);
 		}
