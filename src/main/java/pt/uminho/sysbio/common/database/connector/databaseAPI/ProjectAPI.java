1dace9ddc79abda559c45ff6fd09316cf0f90c47
amaromorais <joseamaromorais@hotmail.com>
2018-04-27 15:40:15 +0100

    prints deletion

--- src/main/java/pt/uminho/sysbio/common/database/connector/databaseAPI/ProjectAPI.java
+++ src/main/java/pt/uminho/sysbio/common/database/connector/databaseAPI/ProjectAPI.java
@@ -438,11 +438,6 @@ public class ProjectAPI {
 				+ " INNER JOIN psort_reports AS pr ON pc.psort_report_id=pr.id "
 				+ " WHERE pr.id='"+id+"' ORDER BY pc.score DESC");
 
-		System.out.println("SELECT c.name, pc.score FROM psort_reports_has_compartments  AS pc "
-				+ " INNER JOIN compartments AS c ON pc.compartment_id=c.id "
-				+ " INNER JOIN psort_reports AS pr ON pc.psort_report_id=pr.id "
-				+ " WHERE pr.id='"+id+"' ORDER BY pc.score DESC");
-		
 		while(rs.next()){
 			String[] list = new String[2];
 			list[0]=rs.getString(1);
